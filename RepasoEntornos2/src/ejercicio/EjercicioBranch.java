22f8c3f33a043f6175e08534c83cee5e79e7200c
isabelkatharinaloerzer <isabelkatharinaloerzer@localhost>
2023-01-25 17:25:39 +0100

    He creado una rama que se llama IsaK

--- RepasoEntornos2/src/ejercicio/EjercicioBranch.java
+++ RepasoEntornos2/src/ejercicio/EjercicioBranch.java
@@ -5,6 +5,8 @@ public class EjercicioBranch {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola, mundo. Me llamo Isabel Katharina Loerzer");
+		
+		System.out.println("Acabo de hacer una rama con mi nombre.");
 	}
 
 }
