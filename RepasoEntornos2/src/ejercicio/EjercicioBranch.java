4787fb7f4ae0d28cc0d89745aa60439a166f22c0
Siobhantt <luisa.cardozo@iesnervion.es>
2023-01-25 17:25:27 +0100

    Pregunta

--- RepasoEntornos2/src/ejercicio/EjercicioBranch.java
+++ RepasoEntornos2/src/ejercicio/EjercicioBranch.java
@@ -5,6 +5,7 @@ public class EjercicioBranch {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola, mundo. Me llamo Isabel Katharina Loerzer");
+		System.out.println("Como se establecia el minimo y el maximo??");
 	}
 
 }
